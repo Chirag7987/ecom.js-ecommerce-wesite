7aa518f0bb546369f03016412d2a1fa041f40798
Chirag Thakur <96881598+Chirag7987@users.noreply.github.com>
2024-03-24 16:17:58 +0530

    Add files via upload

---
+++ updateCartValue.js
@@ -0,0 +1,4 @@
+const cartValue = document.querySelector("#cartValue");
+export const updateCartValue =(cartProducts)=>{
+    return cartValue.innerHTML= `<i class="fa-solid fa-cart-shopping">${cartProducts.length}</i>`;
+}; 
